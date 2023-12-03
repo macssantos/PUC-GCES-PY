a3bae7f5602b7e3df884b4d102a1699769221326
macssantos <mariscosta.contato@gmail.com>
2023-12-03 18:55:59 -0300

    codigo atualizado.

--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,17 @@
-import os
-from parser.YAML_parser import YAMLParser
-from parser.feature_engineering_parser import FeatureEngineeringParser
-from parser.model_parser import ModelParser
+import unittest
+from unittest.mock import patch, mock_open
+from your_module import get_config
 
-def test_get_config():
-    # Crie um arquivo YAML de exemplo em 'src/yamls' para testar a função
-    yaml_content = """
-    feature_engineering:
-      - feature1
-      - feature2
-    model:
-      algorithm: RandomForest
-      parameters:
-        n_estimators: 100
-    """
-    with open('src/yamls/test_config.yaml', 'w') as yaml_file:
-        yaml_file.write(yaml_content)
+class TestGetConfig(unittest.TestCase):
 
-    # Chame a função get_config e verifique se ela retorna os resultados esperados
-    result = get_config()
-    
-    # Adicione suas asserções aqui com base no que você espera
-    assert 'feature1' in result['features_configs']
-    assert 'feature2' in result['features_configs']
-    assert result['model_configs']['algorithm'] == 'RandomForest'
-    assert result['model_configs']['parameters']['n_estimators'] == 100
+    @patch('builtins.open', mock_open(read_data='NUMBER,ADDRESS,ZIPCODE\n124234,EIRUWOEJI,345-345\n124234,EIRUWOEJI,345-345\n124234,EIRUWOEJI,345-345\n'))
+    def test_get_config_with_csv(self):
+        # Chama a função sob teste
+        result = get_config()
 
-    # Limpeza: Remova o arquivo de teste
-    os.remove('src/yamls/test_config.yaml')
+        # Adicione suas asserções com base no que você espera
+        self.assertEqual(result['features_configs'], ['NUMBER', 'ADDRESS', 'ZIPCODE'])
+        # Adicione mais asserções conforme necessário
+
+if __name__ == '__main__':
+    unittest.main()
